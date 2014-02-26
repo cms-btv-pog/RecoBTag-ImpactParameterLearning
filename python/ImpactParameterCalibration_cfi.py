53636f9ec5d391422f680c5a346b6e322151be16
Camille Beluffi <camille.beluffi@cern.ch>
2014-02-26 23:38:06 +0100

    bug fixed

--- python/ImpactParameterCalibration_cfi.py
+++ python/ImpactParameterCalibration_cfi.py
@@ -2,19 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 ipCalib = cms.EDAnalyzer("ImpactParameterCalibration",
                                  
-                                 jetTagsColl              = cms.string("jetProbabilityBJetTags"),
                                  writeToDB                = cms.bool(False),
                                  writeToBinary            = cms.bool(False),
-                                 nBins                    = cms.int32(10000),
+                                 nBins                    = cms.int32(50),
                                  maxSignificance          = cms.double(50.0),
                                  writeToRootXML           = cms.bool(True),
-                                 tagInfoSrc               = cms.InputTag("impactParameterTagInfos"),
                                  inputCategories          = cms.string('HardCoded'),
-                                 primaryVertexSrc         = cms.InputTag("offlinePrimaryVertices"),
-                                 Jets                     = cms.InputTag('selectedPatJetsPF2PAT'),
-                                 jetPModuleName           = cms.string('jetProbabilityBJetTags'),
-                                 produceJetProbaTree      = cms.bool(True),
-                                 jetCorrector             = cms.string('ak5PFL1FastL2L3'),
+                                 primaryVertexColl        = cms.InputTag("goodOfflinePrimaryVertices"),
+                                 Jets                     = cms.InputTag('ak5PFJets'),
                                  MinPt                    = cms.double(10.0),
                                  MaxEta                   = cms.double(2.5)    
                                  
